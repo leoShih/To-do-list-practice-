2d3f23823f9e9e232ee8cbb44fdacd3689a6a915
leoShih <leo201325@gmail.com>
2017-11-26 21:12:36 +0800

    done

--- To do list(practice)/FirstViewController.swift
+++ To do list(practice)/FirstViewController.swift
@@ -8,18 +8,66 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var items:[String] = [] //initially array
+    
+    @IBOutlet var table: UITableView!
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return items.count
+        
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = items[indexPath.row] //to grab items array
+        
+        return cell
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        //check to see if there are object in "itemsObject" ，if have value:
+        if let itemsObject = itemsObject as? [String] {
+            
+            items = itemsObject
+            
+        }
+        
+        table.reloadData()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            
+            items.remove(at: indexPath.row)
+            
+            table.reloadData() //delete items need to refresh tableview
+            
+            UserDefaults.standard.set(items, forKey: "items") //delect items and save data, if not save date the ietms alway be there.
+            
+        }
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
